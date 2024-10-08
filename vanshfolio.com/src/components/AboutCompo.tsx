d6f7224c7aba837473cfb7a94299d961dd352877
vansh kalra <vanshkalra1379@gmail.com>
2024-10-08 23:24:05 +0530

    Adedd some changes

--- vanshfolio.com/src/components/AboutCompo.tsx
+++ vanshfolio.com/src/components/AboutCompo.tsx
@@ -6,10 +6,10 @@ const AboutCompo = () => {
             <span className="text-4xl">💬</span>
             <h1 className="text-3xl font-bold text-gray-700 font-sans">About Me</h1>
             <div className="max-w-4xl">
-                <p className="font-normal mt-4 text-gray-400">Hey there, I'm John Doe - a passionate developer, avid writer, and a connoisseur of awesome design. Welcome to my corner of the digital world!</p>
-                <p className="font-normal mt-4 text-gray-400">Hey there, I'm John Doe - a passionate developer, avid writer, and a connoisseur of awesome design. Welcome to my corner of the digital world!</p>
-                <p className="font-normal mt-4 text-gray-400">Hey there, I'm John Doe - a passionate developer, avid writer, and a connoisseur of awesome design. Welcome to my corner of the digital world!</p>
-                <p className="font-normal mt-4 text-gray-400">Hey there, I'm John Doe - a passionate developer, avid writer, and a connoisseur of awesome design. Welcome to my corner of the digital world!</p>
+                <p className="font-normal mt-4 text-gray-400">Hey there, I'm Vansh Kalra - a passionate developer, avid writer, and a connoisseur of awesome design. Welcome to my corner of the digital world!</p>
+                <p className="font-normal mt-4 text-gray-400">Hey there, I'm Vansh Kalra - a passionate developer, avid writer, and a connoisseur of awesome design. Welcome to my corner of the digital world!</p>
+                <p className="font-normal mt-4 text-gray-400">Hey there, I'm Vansh Kalra - a passionate developer, avid writer, and a connoisseur of awesome design. Welcome to my corner of the digital world!</p>
+                <p className="font-normal mt-4 text-gray-400">Hey there, I'm Vansh Kalra - a passionate developer, avid writer, and a connoisseur of awesome design. Welcome to my corner of the digital world!</p>
             </div>
             <Fotter />
         </div>
