2f776f537f4eb9afae97d96d68d2e115a609d7e3
unknown <vanshkalra1379@gmail.com>
2025-02-25 23:04:29 +0530

    Added tecg stack page

--- vanshfolio.com/src/components/Button.tsx
+++ vanshfolio.com/src/components/Button.tsx
@@ -13,29 +13,29 @@ const Button = () => {
             navigate: "/"
         },
         {
-            id: 2, title: "About", icon: (<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-4 hover:text-blue-500">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 0 1 .865-.501 48.172 48.172 0 0 0 3.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z" />
+            id: 2, title: "About", icon: (<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-4 hover:text-blue-500">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 0 1 .865-.501 48.172 48.172 0 0 0 3.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z" />
             </svg>
             ),
             navigate: "/about"
         },
         {
-            id: 3, title: "Projects", icon: (<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-4 hover:text-blue-500">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 0 0 .75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 0 0-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0 1 12 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 0 1-.673-.38m0 0A2.18 2.18 0 0 1 3 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 0 1 3.413-.387m7.5 0V5.25A2.25 2.25 0 0 0 13.5 3h-3a2.25 2.25 0 0 0-2.25 2.25v.894m7.5 0a48.667 48.667 0 0 0-7.5 0M12 12.75h.008v.008H12v-.008Z" />
+            id: 3, title: "Projects", icon: (<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-4 hover:text-blue-500">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 0 0 .75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 0 0-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0 1 12 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 0 1-.673-.38m0 0A2.18 2.18 0 0 1 3 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 0 1 3.413-.387m7.5 0V5.25A2.25 2.25 0 0 0 13.5 3h-3a2.25 2.25 0 0 0-2.25 2.25v.894m7.5 0a48.667 48.667 0 0 0-7.5 0M12 12.75h.008v.008H12v-.008Z" />
             </svg>
             ),
             navigate: "/project"
         },
         {
-            id: 4, title: "Techstack", icon: (<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-4 hover:text-blue-500">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z" />
+            id: 4, title: "Techstack", icon: (<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-4 hover:text-blue-500">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z" />
             </svg>
             ),
             navigate: "/techstack"
         },
         {
-            id: 5, title: "Contact", icon: (<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-4 hover:text-blue-500">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
+            id: 5, title: "Contact", icon: (<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-4 hover:text-blue-500">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
             </svg>
             ),
             navigate: "/contact"
@@ -69,7 +69,7 @@ const Button = () => {
                         onClick={()=>{window.location.href = "https://x.com/VanshKalra1379"}}
                     >
                         <span className="text-gray-500 mr-3"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-4  hover:text-blue-500">
-                            <path fill-rule="evenodd" d="M5.47 5.47a.75.75 0 0 1 1.06 0L12 10.94l5.47-5.47a.75.75 0 1 1 1.06 1.06L13.06 12l5.47 5.47a.75.75 0 1 1-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 0 1-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd" />
+                            <path fillRule="evenodd" d="M5.47 5.47a.75.75 0 0 1 1.06 0L12 10.94l5.47-5.47a.75.75 0 1 1 1.06 1.06L13.06 12l5.47 5.47a.75.75 0 1 1-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 0 1-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 0 1 0-1.06Z" clipRule="evenodd" />
                         </svg>
                         </span>
                         <span className="text-gray-500">Twitter</span>
@@ -83,7 +83,7 @@ const Button = () => {
                         onClick={() => { window.location.href = "https://www.linkedin.com/in/vansh-kalra-971b5a247/" }}
                     >
                         <span className="text-gray-500 mr-3"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-4  hover:text-blue-500">
-                            <path fill-rule="evenodd" d="M19.902 4.098a3.75 3.75 0 0 0-5.304 0l-4.5 4.5a3.75 3.75 0 0 0 1.035 6.037.75.75 0 0 1-.646 1.353 5.25 5.25 0 0 1-1.449-8.45l4.5-4.5a5.25 5.25 0 1 1 7.424 7.424l-1.757 1.757a.75.75 0 1 1-1.06-1.06l1.757-1.757a3.75 3.75 0 0 0 0-5.304Zm-7.389 4.267a.75.75 0 0 1 1-.353 5.25 5.25 0 0 1 1.449 8.45l-4.5 4.5a5.25 5.25 0 1 1-7.424-7.424l1.757-1.757a.75.75 0 1 1 1.06 1.06l-1.757 1.757a3.75 3.75 0 1 0 5.304 5.304l4.5-4.5a3.75 3.75 0 0 0-1.035-6.037.75.75 0 0 1-.354-1Z" clip-rule="evenodd" />
+                            <path fillRule="evenodd" d="M19.902 4.098a3.75 3.75 0 0 0-5.304 0l-4.5 4.5a3.75 3.75 0 0 0 1.035 6.037.75.75 0 0 1-.646 1.353 5.25 5.25 0 0 1-1.449-8.45l4.5-4.5a5.25 5.25 0 1 1 7.424 7.424l-1.757 1.757a.75.75 0 1 1-1.06-1.06l1.757-1.757a3.75 3.75 0 0 0 0-5.304Zm-7.389 4.267a.75.75 0 0 1 1-.353 5.25 5.25 0 0 1 1.449 8.45l-4.5 4.5a5.25 5.25 0 1 1-7.424-7.424l1.757-1.757a.75.75 0 1 1 1.06 1.06l-1.757 1.757a3.75 3.75 0 1 0 5.304 5.304l4.5-4.5a3.75 3.75 0 0 0-1.035-6.037.75.75 0 0 1-.354-1Z" clipRule="evenodd" />
                         </svg>
                         </span>
                         <span className="text-gray-500">Linkedin</span>
@@ -95,7 +95,7 @@ const Button = () => {
                         className="flex items-center w-full px-4 py-2 text-left hover:rounded-lg hover:shadow-xl hover:bg-white hover:text "
                     >
                         <span className="text-gray-500 mr-3"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-4  hover:text-blue-500">
-                            <path fill-rule="evenodd" d="m6.72 5.66 11.62 11.62A8.25 8.25 0 0 0 6.72 5.66Zm10.56 12.68L5.66 6.72a8.25 8.25 0 0 0 11.62 11.62ZM5.105 5.106c3.807-3.808 9.98-3.808 13.788 0 3.808 3.807 3.808 9.98 0 13.788-3.807 3.808-9.98 3.808-13.788 0-3.808-3.807-3.808-9.98 0-13.788Z" clip-rule="evenodd" />
+                            <path fillRule="evenodd" d="m6.72 5.66 11.62 11.62A8.25 8.25 0 0 0 6.72 5.66Zm10.56 12.68L5.66 6.72a8.25 8.25 0 0 0 11.62 11.62ZM5.105 5.106c3.807-3.808 9.98-3.808 13.788 0 3.808 3.807 3.808 9.98 0 13.788-3.807 3.808-9.98 3.808-13.788 0-3.808-3.807-3.808-9.98 0-13.788Z" clipRule="evenodd" />
                         </svg>
                         </span>
                         <span className="text-gray-500">Youtube</span>
@@ -105,8 +105,8 @@ const Button = () => {
             <div className="pt-10 pl-5 flex items-center pb-7">
                 <button type="button" className="bg-customBlue px-3 py-2.5 rounded-3xl flex items-center justify-center hover:shadow-2xl">
                     <span className=" text-white text-sm  font-semibold">Read Resume</span>
-                    <span className="pl-1"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-4 text-white flex items-center justify-center">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+                    <span className="pl-1"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-4 text-white flex items-center justify-center">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
                     </svg>
                     </span></button>
             </div>
--- vanshfolio.com/src/components/TechCompo.tsx
+++ vanshfolio.com/src/components/TechCompo.tsx
@@ -1,9 +1,181 @@
+import  { useEffect } from 'react';
+import { useState } from 'react';
+
+
+interface TechItem {
+    abbr: string;
+    name: string;
+  }
+  
+  interface TechCategory {
+    title: string;
+    icon: string;
+    items: TechItem[];
+    bubbleClass: string;
+  }
+  
+  interface SkillItem {
+    name: string;
+    level: string;
+    percentage: number;
+    color: string;
+  }
+  
+  interface FactItem {
+    emoji: string;
+    title: string;
+    text: string;
+  }
 
 const TechCompo = () => {
+    
+            const [animate, setAnimate] = useState(false);
+          
+            // Trigger animation after component mounts
+            useEffect(() => {
+              const timer = setTimeout(() => {
+                setAnimate(true);
+              }, 300);
+              return () => clearTimeout(timer);
+            }, []);
+          
+            // Tech categories data
+            const techCategories: TechCategory[] = [
+              {
+                title: "Frontend",
+                icon: "🎨",
+                bubbleClass: "bg-blue-50 text-blue-600",
+                items: [
+                  { abbr: "Ht", name: "HTML5" },
+                  { abbr: "Cs", name: "CSS3" },
+                  { abbr: "Js", name: "JavaScript" },
+                  { abbr: "Re", name: "React" },
+                  { abbr: "Nx", name: "Next.js" },
+                  { abbr: "Tw", name: "Tailwind" }
+                ]
+              },
+              {
+                title: "Backend",
+                icon: "⚙️",
+                bubbleClass: "bg-green-50 text-green-600",
+                items: [
+                  { abbr: "No", name: "Node.js" },
+                  { abbr: "Ex", name: "Express" },
+                  { abbr: "Py", name: "Python" },
+                  { abbr: "Dj", name: "Django" },
+                  { abbr: "Ts", name: "TypeScript" },
+                  { abbr: "Gq", name: "GraphQL" }
+                ]
+              },
+              {
+                title: "Database",
+                icon: "🗄️",
+                bubbleClass: "bg-red-50 text-red-600",
+                items: [
+                  { abbr: "Mg", name: "MongoDB" },
+                  { abbr: "Pg", name: "PostgreSQL" },
+                  { abbr: "My", name: "MySQL" },
+                  { abbr: "Rd", name: "Redis" },
+                  { abbr: "Fb", name: "Firebase" },
+                  { abbr: "Pr", name: "Prisma" }
+                ]
+              },
+              {
+                title: "DevOps",
+                icon: "🚀",
+                bubbleClass: "bg-purple-50 text-purple-600",
+                items: [
+                  { abbr: "Gt", name: "Git" },
+                  { abbr: "Gh", name: "GitHub" },
+                  { abbr: "Dk", name: "Docker" },
+                  { abbr: "Aw", name: "AWS" },
+                  { abbr: "Az", name: "Azure" },
+                  { abbr: "Vr", name: "Vercel" }
+                ]
+              }
+            ];
+    
+            const skills: SkillItem[] = [
+                { name: "Frontend Development", level: "Advanced", percentage: 90, color: "bg-blue-500" },
+                { name: "Backend Development", level: "Proficient", percentage: 85, color: "bg-green-600" },
+                { name: "Database Management", level: "Intermediate", percentage: 75, color: "bg-red-600" },
+                { name: "DevOps & Deployment", level: "Intermediate", percentage: 70, color: "bg-purple-600" },
+                { name: "UI/UX Design", level: "Competent", percentage: 65, color: "bg-orange-500" }
+              ];
+    
+              const facts: FactItem[] = [
+                {
+                  emoji: "💡",
+                  title: "First Stack I Learned",
+                  text: "Started with the MERN stack (MongoDB, Express, React, Node.js) back in 2020. Built my first e-commerce app with it!"
+                },
+                {
+                  emoji: "🔥",
+                  title: "Current Favorite",
+                  text: "Next.js + TypeScript + Tailwind CSS with Prisma for database interactions. It's my go-to for fast, type-safe web development."
+                },
+                {
+                  emoji: "🚀",
+                  title: "Learning Next",
+                  text: "Exploring Rust and WebAssembly to build high-performance web applications. Also diving deeper into AI/ML integration."
+                }
+              ];
     return (
         <div className="mx-40 max-w-4xl py-16 px-6  min-h-screen ">
             <span className="text-4xl">🧑‍💻</span>
             <h1 className="text-3xl font-bold text-gray-700 font-sans mt-1">Techstack I have been working with</h1>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mb-12">
+          {techCategories.map((category) => (
+            <div key={category.title} className="bg-white rounded-xl shadow-sm p-6 transition-transform hover:-translate-y-1 hover:shadow-md relative overflow-hidden">
+              <div className="absolute top-0 left-0 w-full h-1 bg-gradient-to-r from-indigo-500 to-purple-600"></div>
+              
+              <div className="flex justify-between items-center mb-4">
+                <h3 className="text-xl font-semibold text-gray-800">{category.title}</h3>
+                <div className="text-2xl">{category.icon}</div>
+              </div>
+              
+              <div className="grid grid-cols-2 gap-3">
+                {category.items.map((item) => (
+                  <div key={item.name} className="flex items-center gap-2">
+                    <div className={`w-8 h-8 rounded-full ${category.bubbleClass} flex items-center justify-center font-semibold text-sm`}>
+                      {item.abbr}
+                    </div>
+                    <span className="text-sm text-gray-600">{item.name}</span>
+                  </div>
+                ))}
+              </div>
+            </div>
+          ))}
+        </div>
+        
+        {/* Skills Section */}
+        <div className="bg-white rounded-xl shadow-sm p-8 mb-12">
+          <h2 className="text-2xl font-semibold mb-6 text-gray-800">Tech Proficiency</h2>
+          <div className="grid gap-5">
+            {skills.map((skill) => (
+              <div key={skill.name} className="flex flex-col gap-2">
+                <div className="flex justify-between">
+                  <span className="font-medium text-sm">{skill.name}</span>
+                  <span className="text-sm text-gray-600">{skill.level}</span>
+                </div>
+                <div className="h-2 bg-gray-100 rounded-full overflow-hidden">
+                  <div className={`h-full rounded-full transition-all duration-1500 ease-in-out ${skill.color}`} style={{ width: animate ? `${skill.percentage}%` : '0%' }}></div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+        
+        {/* Facts Section */}
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mt-12">
+          {facts.map((fact) => (
+            <div key={fact.title} className="bg-white rounded-xl shadow-sm p-6 flex flex-col gap-3 border-l-4 border-indigo-500">
+              <div className="text-3xl">{fact.emoji}</div>
+              <h3 className="font-semibold text-lg">{fact.title}</h3>
+              <p className="text-gray-600 text-sm leading-relaxed">{fact.text}</p>
+            </div>
+          ))}
+        </div>
         </div>
     )
 }
