9e38d26676c7090f9794ed49690fc9df6296dc65
unknown <vanshkalra1379@gmail.com>
2025-02-27 22:58:24 +0530

    Added sidebar in experince page

---
+++ vanshfolio.com/src/pages/Experience.tsx
@@ -0,0 +1,13 @@
+import Sidebar from "./Sidebar"
+
+export const Experience = () => {
+  return (
+    <div>
+       <div className="flex bg-gray-100">
+            <div>
+                <Sidebar />
+            </div>
+            </div>
+    </div>
+  )
+}
